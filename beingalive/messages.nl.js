d737df5c2bde9a6030e2135bf9e766083f36bf51
bajacobs <34162005+bajacobs@users.noreply.github.com>
2019-11-25 15:56:18 +0100

    Update messages.nl.js
    Kleine aanpassing

--- beingalive/messages.nl.js
+++ beingalive/messages.nl.js
@@ -2,7 +2,7 @@
 
 const MESSAGES = {
     'lang': 'nl',
-    'success': (initials, familyname, dateofbirth) => `De door u onthulde persoonsgegevens <strong>${initials} ${familyname}, geboren op ${dateofbirth}</strong> zijn bij deze geaccepteerd als Attesta di Vita.`,
+    'success': (initials, familyname, dateofbirth) => `De door u onthulde persoonsgegevens <strong>${initials} ${familyname}, geboren op ${dateofbirth}</strong> zijn bij deze geaccepteerd als bewijs van in leven zijn.`,
     'data-too-old': (limit, daysDiff) => `Gegevens voor een Attestatie de Vita mogen hooguit ${limit} dagen oud zijn, maar de door u onthulde gegevens zijn van ${daysDiff} dagen geleden. Ververs de BRP gegevens in uw IRMA app via onderstaande link en probeer het dan opnieuw.`,
     'cancelled': 'Sessie is afgebroken',
 };
