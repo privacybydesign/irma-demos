4f0e46ce5a923b70157fb5e631711cec0811bc5f
Ivar Derksen <ivar.derksen@sidn.nl>
2021-07-28 12:32:54 +0200

    Migrate to irma-frontend

--- start_session.js
+++ start_session.js
@@ -1,29 +1,24 @@
 function start_session(type, lang, success_fun, cancelled_fun, error_fun) {
     console.log("Button clicked");
-    $.get('../start_session.php?type=' + type + '&lang=' + lang, function(sessionpackagejson) {
-        let sessionpackage = JSON.parse(sessionpackagejson);
-        console.log(sessionpackage);
-        let options = {server: sessionpackage.sessionPtr.u.split('/irma')[0], token: sessionpackage.token, language: lang};
-        let promise = irma.handleSession(sessionpackage.sessionPtr, options);
-
-        let success = function (data) {
-            console.log("Session successful!");
-            console.log("Result:", data);
-            success_fun(data);
-        };
-
-        let error = function(data) {
-            if(data === 'CANCELLED') {
-                console.log("Session cancelled!");
-                cancelled_fun(data);
+    irma.newPopup({
+        language: lang,
+        session: {
+            url: '..',
+            start: {
+                url: o => `${o.url}/start_session.php?type=${type}&lang=${lang}`
+            },
+            result: {
+                url: (o, {sessionPtr, sessionToken}) => `${sessionPtr.u.split('/irma')[0]}/session/${sessionToken}/result`
             }
-            else {
-                console.log("Session failed!");
-                console.log("Error data:", data);
-                error_fun(data);
+        }
+    })
+        .start()
+        .then(success_fun)
+        .catch(function (msg) {
+            if (msg === 'Cancelled' || msg === 'Aborted') {
+                cancelled_fun(msg);
+            } else {
+                error_fun(msg);
             }
-        };
-
-        promise.then(success, error);
-    });
+        });
 }
