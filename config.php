41d7ba141f405dfa2f695546740ea049453de604
saravahdatipour <verpiderp@gmail.com>
2024-12-03 16:29:40 +0100

    revert to use stored public key

--- config.php
+++ config.php
@@ -2,21 +2,21 @@
  
 define('IRMA_SERVER_URL', getenv('IRMA_SERVER_URL') ?: 'http://localhost:8088');
 define('API_TOKEN', getenv('API_TOKEN') ?: '');
-define('DEMO', false);
-define('JWT_ENABLED', true);
+define('DEMO', true);
+define('JWT_ENABLED', getenv('JWT_ENABLED')?: false);
 
 if (DEMO) {
     define('ISSUER', 'irma-demo');
-    define('BASE_URL', 'http://localhost');
-    define('IRMATUBE_NEXT_SESSION_URL', BASE_URL . '/nl/get_session_request.php');
+    define('BASE_URL', 'http://localhost:8080'); 
+    define('IRMATUBE_NEXT_SESSION_URL', BASE_URL . '/get_session_request.php');
     define('IRMATUBE_CREDENTIAL', 'irma-demo.IRMATube.member');
 } else {
-    define('ISSUER', 'pbdf');
-    define('BASE_URL', 'https://privacybydesign.foundation');
-    define('IRMATUBE_NEXT_SESSION_URL', BASE_URL . '/demo/get_session_request.php');
-    define('IRMATUBE_CREDENTIAL', 'pbdf.pbdf.irmatube');
+    define('ISSUER',getenv('ISSUER') ?: 'pbdf');
+    define('BASE_URL', getenv('BASE_URL') ?: 'https://privacybydesign.foundation');
+    define('IRMATUBE_NEXT_SESSION_URL',getenv('IRMATUBE_NEXT_SESSION_URL') ?: BASE_URL . '/get_session_request.php');
+    define('IRMATUBE_CREDENTIAL',  getenv('IRMATUBE_CREDENTIAL') ?: 'pbdf.pbdf.irmatube');
 }
 
 if (JWT_ENABLED) {
-    define('IRMA_SERVER_PUBLICKEY', __DIR__ . '/../data/pk.pem');
+    define('IRMA_SERVER_PUBLICKEY',getenv('IRMA_SERVER_PUBLICKEY') ?: __DIR__ . '/../data/pk.pem');
 }
