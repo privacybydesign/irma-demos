2e4a7babe7ccd79439f076a9fb7b0dbaaafcdae0
saravahdatipour <verpiderp@gmail.com>
2025-01-16 13:42:08 +0100

    IRMA to Yivi

--- demos/beingalive/messages.en.js
+++ demos/beingalive/messages.en.js
@@ -3,6 +3,6 @@
 const MESSAGES = {
     'lang': 'en',
     'success': (initials, familyname, dateofbirth) => `The data that you revealed: <strong>${initials} ${familyname}, born on ${dateofbirth}</strong> are hereby accepted as proof of being alive.`,
-    'data-too-old': (limit, daysDiff) => `Data for an Attestatio de Vita can be at most ${limit} days old, but the data that you revealed are from ${daysDiff} days ago. Refresh the BRP data in your IRMA app via the link below and try again.`,
+    'data-too-old': (limit, daysDiff) => `Data for an Attestatio de Vita can be at most ${limit} days old, but the data that you revealed are from ${daysDiff} days ago. Refresh the BRP data in your Yivi app via the link below and try again.`,
     'cancelled': 'Session has been cancelled',
 };
