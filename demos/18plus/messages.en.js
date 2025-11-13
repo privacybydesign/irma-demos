97ebaa901ecbe474cdd96401da7b57b35a453472
Dibran Mulder <d.mulder@caesar.nl>
2025-11-13 19:42:12 +0100

    Updated 18plus scenario

--- demos/18plus/messages.en.js
+++ demos/18plus/messages.en.js
@@ -3,7 +3,7 @@
 const MESSAGES = {
     'lang':                     'en',
     'cancel-message':           '18+ check cancelled.',
-    'age-check-succeeded':      '<h3>18+ Age check succeeded!</h3><p><em>Grand Theft Auto</em> (GTA) is an example of a game with an age limit of 18+. After this successful age check GTA can be obtained in accordance with regulation.</p>',
+    'age-check-succeeded':      '<h3>18+ Age check succeeded!</h3><p>You are over 18 but unfortunately GTA VI is again postponed.</p>',
     'age-check-not-succeeded':  '<h3>18+ Age check not succeeded!</h3><p>Unfortunately, we identified you as being a minor. Therefore we cannot show you the content.</p>',
     'error-message':            '18+ verification failed!',
     'back':                     'Back'
--- demos/18plus/messages.nl.js
+++ demos/18plus/messages.nl.js
@@ -3,7 +3,7 @@
 const MESSAGES = {
     'lang':                     'nl',
     'cancel-message':           '18+ check geannuleerd.',
-    'age-check-succeeded':      '<h3>18+ leeftijdscontrole geslaagd!</h3><p><em>Grand Theft Auto</em> (GTA) is een voorbeeld van een game met een leeftijdsgrens van 18+. GTA kan na deze geslaagde leeftijdscontrole volgens de regels aangeschaft worden.</p>',
+    'age-check-succeeded':      '<h3>18+ leeftijdscontrole geslaagd!</h3><p>Je bent ouder dan 18, maar helaas is GTA VI opnieuw uitgesteld.</p>',
     'age-check-not-succeeded':  '<h3>18+ leeftijdscontrole is niet geslaagd</h3><p>Helaas, u bent nog geen 18 jaar. Daarom kunnen we u de inhoud niet laten zien.</p>',
     'error-message':            '18+ check mislukt!',
     'back':                     'Terug'
