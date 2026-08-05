5e7ed97c56139b597f6437fa50789ba42cb8d247
Floris Jansen <florismartijnjansen@gmail.com>
2026-08-05 13:17:54 +0200

    load start_session from html, not js

--- demos/18plus/18plusdemo.js
+++ demos/18plus/18plusdemo.js
@@ -1,34 +1,32 @@
-$.getScript('/start_session.js', function() {
-    let success_fun = function(data) {
-        let attr = data.disclosed[0][0].rawvalue.toLowerCase();
-        if(attr === 'yes' || attr === 'ja') {
-            $('#main').html(MESSAGES['age-check-succeeded'] +
-                '<div style="text-align: center"><img src=\"GTAVI.png\" alt="GTA VI image"></div> <br> <p><a href=\"#\" ' +
-                'onclick=\"window.location.reload(true)\">' + MESSAGES['back'] + '</a></p>');
-        }
-        else {
-            $('#main').html(MESSAGES['age-check-not-succeeded'] +
-                '<br><p><a href=\"#\" onclick=\"window.location.reload(true)\">' + MESSAGES['back'] + '</a></p>');
-        }
-    };
+let success_fun = function(data) {
+    let attr = data.disclosed[0][0].rawvalue.toLowerCase();
+    if(attr === 'yes' || attr === 'ja') {
+        $('#main').html(MESSAGES['age-check-succeeded'] +
+            '<div style="text-align: center"><img src=\"GTAVI.png\" alt="GTA VI image"></div> <br> <p><a href=\"#\" ' +
+            'onclick=\"window.location.reload(true)\">' + MESSAGES['back'] + '</a></p>');
+    }
+    else {
+        $('#main').html(MESSAGES['age-check-not-succeeded'] +
+            '<br><p><a href=\"#\" onclick=\"window.location.reload(true)\">' + MESSAGES['back'] + '</a></p>');
+    }
+};
 
-    let cancel_fun = function() {
-        $('#result_status')
-            .html(MESSAGES['cancel-message'])
-            .addClass('alert alert-warning')
-            .css('font-weight', 'bold');
-    };
+let cancel_fun = function() {
+    $('#result_status')
+        .html(MESSAGES['cancel-message'])
+        .addClass('alert alert-warning')
+        .css('font-weight', 'bold');
+};
 
-    let error_fun = function() {
-        $('#result_status')
-            .html(MESSAGES['error-message'])
-            .addClass('alert alert-danger')
-            .css('font-weight', 'bold');
-    };
+let error_fun = function() {
+    $('#result_status')
+        .html(MESSAGES['error-message'])
+        .addClass('alert alert-danger')
+        .css('font-weight', 'bold');
+};
 
-    $('#try_irma_18btn').click(function() {
-        $('#result_status').removeClass().html();
-        start_session('18plus', MESSAGES['lang'], success_fun, cancel_fun, error_fun);
-    });
-
+$('#try_irma_18btn').click(function() {
+    $('#result_status').removeClass().html();
+    start_session('18plus', MESSAGES['lang'], success_fun, cancel_fun, error_fun);
 });
+
--- demos/student/studentdemo.js
+++ demos/student/studentdemo.js
@@ -1,44 +1,42 @@
-$.getScript('/start_session.js', function() {
-    let successstudent_fun = function (data) {
-        let result = data.disclosed[0][0].rawvalue;
-        if (result === 'student') {
-            $('#main').html(MESSAGES['succeeded-student'] + '<br> <p><a href="#" onclick="window.location.reload(true)">' +
-                MESSAGES['back'] + '</a></p>');
-        } else {
-            $('#main').html(MESSAGES['failed-student'] + '<br> <p><a href="#" onclick="window.location.reload(true)">' +
-                MESSAGES['back'] + '</a></p>');
-        }
-    };
+let successstudent_fun = function (data) {
+    let result = data.disclosed[0][0].rawvalue;
+    if (result === 'student') {
+        $('#main').html(MESSAGES['succeeded-student'] + '<br> <p><a href="#" onclick="window.location.reload(true)">' +
+            MESSAGES['back'] + '</a></p>');
+    } else {
+        $('#main').html(MESSAGES['failed-student'] + '<br> <p><a href="#" onclick="window.location.reload(true)">' +
+            MESSAGES['back'] + '</a></p>');
+    }
+};
 
 
-    let successschool_fun = function (data) {
-        let role = data.disclosed[0][0].rawvalue;
-        let school = data.disclosed[0][1].rawvalue;
-        $('#main')
-            .empty()
-            .append(MESSAGES['succeeded-school'](role, school))
-            .append('<br><p><a href=\"#\" onclick=\"window.location.reload(true)\">' + MESSAGES['back'] + '</a></p>');
-    };
+let successschool_fun = function (data) {
+    let role = data.disclosed[0][0].rawvalue;
+    let school = data.disclosed[0][1].rawvalue;
+    $('#main')
+        .empty()
+        .append(MESSAGES['succeeded-school'](role, school))
+        .append('<br><p><a href=\"#\" onclick=\"window.location.reload(true)\">' + MESSAGES['back'] + '</a></p>');
+};
 
-    let cancel_fun = function () {
-        $('#result_status')
-            .html(MESSAGES['cancel-message'])
-            .addClass('alert alert-warning')
-            .css('font-weight', 'bold');
-    };
+let cancel_fun = function () {
+    $('#result_status')
+        .html(MESSAGES['cancel-message'])
+        .addClass('alert alert-warning')
+        .css('font-weight', 'bold');
+};
 
-    let error_fun = function () {
-        $('#result_status')
-            .html(MESSAGES['error-message'])
-            .addClass('alert alert-danger')
-            .css('font-weight', 'bold');
-    };
+let error_fun = function () {
+    $('#result_status')
+        .html(MESSAGES['error-message'])
+        .addClass('alert alert-danger')
+        .css('font-weight', 'bold');
+};
 
-    $('#try_irma_studentbtn').click(function () {
-        start_session('student', MESSAGES['lang'], successstudent_fun, cancel_fun, error_fun);
-    });
+$('#try_irma_studentbtn').click(function () {
+    start_session('student', MESSAGES['lang'], successstudent_fun, cancel_fun, error_fun);
+});
 
-    $('#try_irma_studentschoolbtn').click(function () {
-        start_session('school', MESSAGES['lang'], successschool_fun, cancel_fun, error_fun);
-    });
+$('#try_irma_studentschoolbtn').click(function () {
+    start_session('school', MESSAGES['lang'], successschool_fun, cancel_fun, error_fun);
 });
