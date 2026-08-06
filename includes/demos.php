d30a51dadb6021f8b7f86fdecdc52d242245f8a3
Floris Jansen <florismartijnjansen@gmail.com>
2026-08-06 13:23:04 +0200

    add demo titles to separate file

---
+++ includes/demos.php
@@ -0,0 +1,39 @@
+<?php
+$demos = [
+    "home" => [
+        "en" => "Yivi demos",
+        "nl" => "Yivi demos"
+    ],
+    "18plus" => [
+        "en" => "Prove 18+ with Yivi",
+        "nl" => "18+ bewijzen met Yivi"
+    ],
+    "address" => [
+        "en" => "Address form-filling with Yivi",
+        "nl" => "Adresformulier invullen met Yivi"
+    ],
+    "beingalive" => [
+        "en" => "Prove that you are alive with Yivi",
+        "nl" => "Bewijs dat je leeft met Yivi"
+    ],
+    "iban" => [
+        "en" => "IBAN form-filling with Yivi",
+        "nl" => "IBAN-formulier invullen met Yivi"
+    ],
+    "irmaTubePremium" => [
+        "en" => "Chained sessions with Yivi",
+        "nl" => "Chained sessions met Yivi"
+    ],
+    "mail" => [
+        "en" => "Verification of email address with Yivi",
+        "nl" => "Controle van e-mailadres met Yivi"
+    ],
+    "signature" => [
+        "en" => "Digital signatures with Yivi",
+        "nl" => "Digitale handtekeningen met Yivi"
+    ],
+    "student" => [
+        "en" => "Access for students with Yivi",
+        "nl" => "Toegang voor studenten met Yivi"
+    ]
+];
