3d576b6e100ca1700b46fe480165cb9dfa4a569d
Floris Jansen <florismartijnjansen@gmail.com>
2026-08-06 11:37:57 +0200

    convert main index to php version

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+$title = array(
+		"en" => "Yivi demos",
+		"nl" => "Yivi demos"
+);
+include($_SERVER['DOCUMENT_ROOT'] . '/includes/head.php');
+include($_SERVER['DOCUMENT_ROOT'] . '/includes/header.php');
+include("./$lang.php");
+include($_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php');
