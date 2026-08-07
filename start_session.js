757e852666ed1ca75c2185eccf84533f5699622b
Floris Jansen <florismartijnjansen@gmail.com>
2026-08-07 16:04:23 +0200

    add option to start yivi session inline

--- start_session.js
+++ start_session.js
@@ -21,3 +21,41 @@ function start_session(type, lang, success_fun, cancelled_fun, error_fun) {
             }
         });
 }
+
+
+function start_session_inline(type, lang, verifier) {
+
+    let showResult = (state, message) => {
+        let div = document.createElement('div');
+        div.innerHTML = message;
+        div.classList.add(state, 'yivi-result');
+        document.querySelector('.demo-container').append(div)
+    }
+
+    yivi.newWeb({
+        language: lang,
+        element: '#yivi-web-form',
+        translations: {
+            header: header_text,
+        },
+        session: {
+            start: {
+                url: o => `${o.url}/start_session.php?type=${type}&lang=${lang}`
+            },
+            result: {
+                url: (o, {sessionPtr, sessionToken}) => `${sessionPtr.u.split('/irma')[0]}/session/${sessionToken}/result`
+            }
+        }
+    })
+        .start()
+        .then((data) => {
+            showResult('result', verifier(data));
+        })
+        .catch((data) => {
+            if (data === 'Cancelled' || data === 'Aborted') {
+                showResult('ok', 'You cancelled the action. You can try again!')
+            } else {
+                showResult('error', 'Something went wrong, please try again later.');
+            }
+        });
+}
