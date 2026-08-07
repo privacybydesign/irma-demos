4ac17e706e67157918b06fc449efc8508f4c2e05
Floris Jansen <florismartijnjansen@gmail.com>
2026-08-07 12:08:58 +0200

    add header with logo and links

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,21 @@
 <body>
 	<header>
-		<h1><?php echo $title; ?></h1>
+		<div>
+			<a href="/" class="logo">
+				<img height="28" src="/resources/images/yivi-logo.svg" alt="Yivi">
+				Demos
+			</a>
+			<nav>
+				<ul>
+					<li>
+						<a href="https://docs.yivi.app" target="_blank">Docs</a>
+					</li>
+					<li>
+						<a href="https://yivi.app" target="_blank">Main</a>
+					</li>
+				</ul>
+			</nav>
+		</div>
 	</header>
 	<div class="container mt-5" id="main">
 		<div id="result_status"></div>
