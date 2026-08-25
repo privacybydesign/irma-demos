02a5a8dd4169ece03e9b59878fe255e4c413a463
Floris Jansen <florismartijnjansen@gmail.com>
2026-08-25 13:20:49 +0200

    transform beingalive to new page

--- demos/beingalive/en.php
+++ demos/beingalive/en.php
@@ -1,42 +1,19 @@
-
-<p>
-    Via this page you can, simply and without costs, produce
-    evidence of "being alive". Certain (live)insurances ask
-    regularly for such a <em>Attestatio de Vita</em>.
-</p>
-
-<div id="result" class="alert" hidden></div>
-
-<h3>Reveal personal data</h3>
-<p>
-    Via the button below you will be asked to reveal via your Yivi
-    app the following data: your family name, initials and data of
-    birth. It will be checked whether they are at most 14 days
-    old, that is, that they have been loaded into your Yivi app
-    less than 14 days ago.
-</p>
-
-<div style="text-align: center; margin-bottom: 25px">
-    <button class="custom-button" id="verification">Reveal data for Attestatio de Vita</button>
-</div>
-
-<h3>Background</h3>
-<ul>
-    <li>You can reveal the requested data via
-        the <a href="https://yivi.app/#download">Yivi
-            app</a> on your mobile phone.</li>
-    <li>You need to have already loaded the relevant personal data
-        in your Yivi app.  In the Netherlands these data can be
-        obtained online from government registers via
-        municipalities.</li>
-    <li>These so-called BRP data can be loaded (or refreshed) into
-        your Yivi app
-        via <a href="https://yivi.nijmegen.nl/login">this
-        link</a>.</li>
-    <li>After this demo your data will be removed immediately.
-</ul>
-
-<p>
-<a href="../../">Back</a> to
-the Yivi demo overview.
-</p>
+<?php
+$content = [
+    "intro" => "Useful to prove you are alive for life annuities or pensions.",
+    "benefits" => [
+        "Saves you a trip to the city hall",
+        "No physical forms which need to be sent by post"
+    ],
+    "data" => [
+        "description" => "The personal data card",
+        "sources" => [
+            [
+                "url" => "https://yivi.nijmegen.nl/login",
+                "label" => "the Dutch resident registration (BRP)"
+            ]
+        ]
+    ],
+    "action" => "Prove you are alive",
+    "sidenotes" => "Pension funds or governments giving out annuities sometimes require a certificate of life. Traditionally, this requires getting a signed form at a counter. By sharing identifying details which were recently loaded into Yivi, the receiving party can be sure the person is alive. After all, the resident registration will not allow the card to be issued if the person has passed."
+];
--- demos/beingalive/script.js
+++ demos/beingalive/script.js
@@ -1,52 +1,40 @@
-function doVerificationSession() {
-    const e = document.getElementById('result');
-    e.classList.forEach((label) => {
-        if (label.includes('-'))
-            e.classList.remove(label)
-    });
-    start_session('beingalive', MESSAGES['lang'], onSuccess, showError, showError);
-}
+let verifier = (data) => {
+    let issuanceTime = data.disclosed[0][0].issuancetime;
+    const values = {
+        'initials': data.disclosed[0][0].rawvalue,
+        'familyName': data.disclosed[0][1].rawvalue,
+        'dateOfBirth': data.disclosed[0][2].rawvalue,
+    }
 
-function onSuccess(result) {
     const limit = 14;
     const now = new Date();
-    const microSecondsDiff = Math.floor(now.getTime() - 1000 * result.disclosed[0][0].issuancetime);
-    // Number of milliseconds per day =
-    // 24 hrs/day * 60 minutes/hour * 60 seconds/minute * 1000 msecs/second
-    const daysDiff = Math.floor(microSecondsDiff / (1000 * 60 * 60 * 24));
-    console.log("Day difference " + daysDiff);
-    console.log(result.disclosed);
-    if (daysDiff <= limit) {
-        showSuccess(MESSAGES['success'](result.disclosed[0][0].rawvalue, result.disclosed[0][1].rawvalue, result.disclosed[0][2].rawvalue));
+    console.log(issuanceTime);
+    const dataAge = Math.floor((now.getTime() - 1000 * issuanceTime) / (1000 * 60 * 60 * 24));
+
+    document.querySelector('.yivi-web-form').remove();
+    document.querySelector('.result').removeAttribute('hidden');
+
+    document.getElementById('dataAge').innerText = dataAge;
+
+    if (dataAge < limit) {
+        const success = document.querySelector('.success');
+        success.removeAttribute('hidden');
+        for (let item in values) {
+            document.getElementById(item).innerText = values[item];
+        }
+
     } else {
-        console.log("Failure");
-        showError(MESSAGES['data-too-old'](limit, daysDiff));
+        const error = document.querySelector('.error');
+        error.removeAttribute('hidden');
+        document.getElementById('limit').innerText = limit;
+
     }
-}
 
-function showError(err) {
-    const e = document.getElementById('result');
-    e.removeAttribute('hidden');
-    e.classList.forEach((label) => {
-        if (label.includes('-'))
-            e.classList.remove(label)
-    });
+    return false;
+};
 
-    if (err === 'CANCELLED') {
-        e.classList.add('alert-warning');
-        e.innerText = MESSAGES['cancelled'];
-    } else {
-        e.classList.add('alert-danger');
-        e.innerText = String(err);
-    }
-    throw err;
-}
-
-function showSuccess(node) {
-    const e = document.getElementById('result');
-    e.replaceChildren(node);
-    e.removeAttribute('hidden');
-    e.classList.add('alert-success');
-}
-
-document.getElementById('verification').addEventListener('click', doVerificationSession);
+start_session_inline(slug, lang, verifier, (message) => {
+    let error = document.createElement('p');
+    error.innerText = message;
+    document.querySelector('.demo-figure').after(error);
+});
