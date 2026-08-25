bbd94faf2a965ea9328addaf3a0b52cabe087aad
Floris Jansen <florismartijnjansen@gmail.com>
2026-08-25 09:26:27 +0200

    move top and bottom of demo to includes

--- demos/address/demo.php
+++ demos/address/demo.php
@@ -22,46 +22,17 @@ $demo_strings = [
     ]
 ];
 
-if ($render_full_page):
-    include($_SERVER['DOCUMENT_ROOT'] . "/includes/lang.php"); ?>
-<!DOCTYPE html>
-<html lang="<?php echo $lang; ?>">
-<head>
-    <meta name="viewport" content="width=device-width,initial-scale=1">
-    <meta charset="utf-8">
-
-	<link href="/resources/vars.css" rel="stylesheet">
-	<link href="/resources/yivi.css" rel="stylesheet">
-	<link href="/resources/demo.css" rel="stylesheet">
-
-	<script src="/assets/yivi.js" defer></script>
-	<script src="/start_session.js" defer></script>
-	<script src="./script.js" defer></script>
-
-    <title><?php echo $title; ?> | Yivi Demos</title>
-
-	<script type="text/javascript">
-		let header_text = '<?php echo $demo_strings['action'][$lang]; ?>';
-		let lang = '<?php echo $lang; ?>';
-		let slug = '<?php echo $demo_strings['slug']; ?>';
-	</script>
-</head>
-<body class="stand-alone">
-
-<?php endif; ?>
+include($_SERVER['DOCUMENT_ROOT'] . "/includes/demo-head.php"); ?>
 
 <style>
-	 body {
-         --accent: #712F87;
-	 }
-    .address-figure {
-        --accent: #712F87;
-        --secondary: #F7E30F;
+	body {
+		--accent: #712F87;
+		--secondary: #F7E30F;
 	}
 	.address-usage {
-        padding: calc(1em + .5vw) calc(1.5em + 1vw);
-        background: oklch(from white l c h / calc(alpha - .8));
-        border-radius: 12px;
+		padding: calc(1em + .5vw) calc(1.5em + 1vw);
+		background: oklch(from white l c h / calc(alpha - .8));
+		border-radius: 12px;
 
 		p {
 			margin-block: 12px;
@@ -69,49 +40,31 @@ if ($render_full_page):
 		}
 
 		:first-child {
-            font-size: max(3.5vw, 2em);
-        }
+			font-size: max(3.5vw, 2em);
+		}
 
-        .number {
-            font-size: 1.2em;
-            font-weight: 900;
-        }
+		.number {
+			font-size: 1.2em;
+			font-weight: 900;
+		}
 
 		.year-at {
-            text-transform: uppercase;
-            font-size: 1em;
-            letter-spacing: .1em;
-            font-weight: 600;
+			text-transform: uppercase;
+			font-size: 1em;
+			letter-spacing: .1em;
+			font-weight: 600;
 		}
-    }
+	}
 </style>
 
-<figure class="address-figure demo">
+<div class="address-usage" hidden>
+	<p>
+		<span class="number">942</span>
+		<span class="unit">kWh</span>
+	</p>
+	<p class="year-at">
+		<?php echo $demo_strings['year'][$lang]; ?>
+	</p>
+</div>
 
-<header class="demo-header">
-    <div class="demo-logo">
-        <?php echo $demo_strings['organisation'][$lang]; ?>
-    </div>
-</header>
-
-<<?php echo ($render_full_page) ? 'main' : 'section'; ?> class="demo-main">
-    <h<?php echo ($render_full_page) ? '1' : '2'; ?>>
-        <?php echo $demo_strings['title'][$lang]; ?>
-    </h<?php echo ($render_full_page) ? '1' : '2'; ?>>
-    <div id="yivi-web-form"></div>
-	<div class="address-usage" hidden>
-		<p>
-			<span class="number">942</span>
-			<span class="unit">kWh</span>
-		</p>
-		<p class="year-at">
-			<?php echo $demo_strings['year'][$lang]; ?>
-		</p>
-	</div>
-</<?php echo ($render_full_page) ? 'main' : 'section'; ?>>
-
-</figure>
-
-<?php if ($render_full_page): ?>
-</body>
-<?php endif; ?>
+<?php include($_SERVER['DOCUMENT_ROOT'] . "/includes/demo-foot.php"); ?>
---
+++ includes/demo-foot.php
@@ -0,0 +1,7 @@
+</<?php echo ($render_full_page) ? 'main' : 'section'; ?>>
+
+</figure>
+
+<?php if ($render_full_page): ?>
+    </body>
+<?php endif; ?>
