718e09f436ef4c8839ba018604cfb3f27364758a
Floris Jansen <florismartijnjansen@gmail.com>
2026-08-26 12:43:11 +0200

    remove obsolete message js loading

--- includes/head.php
+++ includes/head.php
@@ -13,11 +13,7 @@
 
         <script src="/assets/yivi.js" defer></script>
 		<script src="/start_session.js" defer></script>
-<?php // Both are per-demo files, relative to the page being rendered. ?>
-<?php if (file_exists("messages.$lang.js")): ?>
-        <script src="messages.<?php echo $lang; ?>.js" defer></script>
-<?php endif; ?>
-<?php if (file_exists('script.js')): ?>
-        <script src="./script.js" defer></script>
-<?php endif; ?>
+		<?php if (file_exists('script.js')): ?>
+        	<script src="./script.js" defer></script>
+		<?php endif; ?>
     </head>
