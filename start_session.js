5ed2dc0119eeec5912b6489d0b9c8fc956a1770f
Floris Jansen <florismartijnjansen@gmail.com>
2026-08-26 12:28:43 +0200

    remove now obsolete start session function

--- start_session.js
+++ start_session.js
@@ -1,28 +1,3 @@
-function start_session(type, lang, success_fun, cancelled_fun, error_fun) {
-    console.log("Button clicked");
-    yivi.newPopup({
-        language: lang,
-        session: {
-            start: {
-                url: o => `${o.url}/start_session.php?type=${type}&lang=${lang}`
-            },
-            result: {
-                url: (o, {sessionPtr, sessionToken}) => `${sessionPtr.u.split('/irma')[0]}/session/${sessionToken}/result`
-            }
-        }
-    })
-        .start()
-        .then(success_fun)
-        .catch(function (msg) {
-            if (msg === 'Cancelled' || msg === 'Aborted') {
-                cancelled_fun(msg);
-            } else {
-                error_fun(msg);
-            }
-        });
-}
-
-
 const INLINE_SESSION_MESSAGES = {
     cancelled: {
         en: 'You cancelled the action. You can try again!',
